1f16fd5aa245d3daf1ce95766637cd63611afdf3
Jatin Verma <jverma310704@gmail.com>
2026-08-11 15:56:33 +0530

    Delete RetailBrainOS/edge-runtime directory

--- RetailBrainOS/edge-runtime/app/services/__init__.py
+++
@@ -1,9 +0,0 @@
-from app.services.camera_service import CameraService
-from app.services.health_service import HealthService
-from app.services.stream_service import StreamService
-
-__all__ = [
-    "CameraService",
-    "StreamService",
-    "HealthService",
-]
